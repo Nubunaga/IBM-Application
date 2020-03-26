2dd464479c0fda95cfb193e0f9e4c91a3e150ae5
Nubunaga <43177566+Nubunaga@users.noreply.github.com>
2020-03-26 11:18:54 +0100

    v1

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var app = express();
 var http = require('http').Server(app);
 var ws = require("socket.io")(http);
 var cfenv = require('cfenv');
-var IoTApp  = require('./application/application.js');
+var IoTApp = require('./application/application.js');
 
 /* Serve the files out of ./public as our main files. */
 app.use(express.static(__dirname + '/public'));
@@ -14,18 +14,18 @@ app.use(express.static(__dirname + '/public'));
   cfenv will use the file defined by vcapFile instead.
   You can export these local json files from IBM Cloud!
 */
-var app_env = cfenv.getAppEnv({vcapFile: 'vcap.json'});
-const IOT_PLATFORM = "NAME";
+var app_env = cfenv.getAppEnv({ vcapFile: 'vcap.json' });
+const IOT_PLATFORM = "Internet of Things Platform-5f";
 
 /* Retrieve Cloud Foundry environment variables. */
 var credentials = app_env.getServiceCreds(IOT_PLATFORM);
 var application = new IoTApp(credentials.org, credentials.apiKey, credentials.apiToken);
 
 /* Application is an event emitter, so we listen for the payload event we defined in application.js! */
-application.on('payload', function(data) {
+application.on('payload', function (data) {
   /* We then broadcast to our clients.  */
   ws.emit('broadcast', JSON.parse(data).temp);
 });
 
 /* Start server on the specified port and binding host app_env.port */
-http.listen(app_env.port || 4096, function() {});
+http.listen(app_env.port || 4096, function () { });
